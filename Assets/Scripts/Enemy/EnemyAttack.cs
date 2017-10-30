5b18f4405603409712797b2876730c24b5b6b05d
xkr0 <cros.camille31@gmail.com>
2017-10-30 19:17:04 -0400

    enemy moves towards the player and attacks it

---
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 1f;
+    public int attackDamage = 10;
+
+    public int numberOfAttacks = 2;
+    private int chooseAttack = 0;
+
+
+    Animator anim;
+    GameObject player;
+    //PlayerHealth playerHealth;
+    //EnemyHealth enemyHealth;
+    bool playerInRange;
+    float timer;
+
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        //playerHealth = player.GetComponent<PlayerHealth>();
+        //enemyHealth = GetComponent<EnemyHealth>();
+        anim = GetComponent<Animator>();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = true;
+            anim.SetBool("PlayerInRange", true);
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = false;
+            anim.SetBool("PlayerInRange", false);
+        }
+    }
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= timeBetweenAttacks && playerInRange /* && enemyHealth.currentHealth > 0 */)
+        {
+            Attack();
+        }
+
+        /*
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("PlayerDead");
+        }
+        */
+    }
+
+
+    void Attack()
+    {
+        timer = 0f;
+        anim.SetInteger("NumAttack", chooseAttack);
+        chooseAttack = (++chooseAttack) % numberOfAttacks;
+
+        /*
+        if (playerHealth.currentHealth > 0)
+        {
+            playerHealth.TakeDamage(attackDamage);
+        }
+        */
+    }
+}
