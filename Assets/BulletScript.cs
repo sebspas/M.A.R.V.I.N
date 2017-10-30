0be1d1a1c6733109dca0fc9f1f4271f3bc77bb9f
sebspas <sebspas@gmail.com>
2017-10-30 18:55:03 -0400

    Player sound + player movement all redone + bullet destroy on collision or on timer (4sec)

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Destroy(this.gameObject, 4);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //print(other.tag);
+        if (other.tag == "Player")
+        {
+            // don't destroy it when it's the player shooting (or the bullet will never go out of the player collider...)
+        } else
+        {
+            // in all other case destroy it
+            Destroy(this.gameObject);
+        }
+       
+    }
+}
