26ebc6de311efb2dff83557e94e622030b63cfb4
louis2580 <louis0710@live.fr>
2017-11-10 20:09:14 -0500

    animation du crystal à la fin de la récolte, gestion du dévérouillage des armes, j'ai essayé de mettre une animation sur le UI, sans succès

--- Assets/Scripts/CrystalMining.cs
+++ Assets/Scripts/CrystalMining.cs
@@ -13,6 +13,14 @@ public class CrystalMining : MonoBehaviour {
 
     public GameObject flyingCrystal;
 
+    // End of crystal animations:
+    public GameObject crystalAura;
+    //public GameObject crystalEnv;
+
+    // list of all the enemies when the crystal is ended
+    public GameObject[] allEnemies;
+    public GameObject fire;
+
     // the crystal sphere collider
     SphereCollider sc;
 
@@ -23,6 +31,7 @@ public class CrystalMining : MonoBehaviour {
     float miningSpeed;
     // The amont of cristal per timeBetweenCristal the player is mining
     float miningAmont;
+    bool isEmpty;
 
 
     // Use this for initialization
@@ -36,6 +45,7 @@ public class CrystalMining : MonoBehaviour {
 
         // Create the sphere collider, radius = 2, isTrigger = true (we can go through it)
         sc = gameObject.GetComponent<SphereCollider>() as SphereCollider;
+        isEmpty = false;
     }
 	
 	// Update is called once per frame
@@ -44,18 +54,27 @@ public class CrystalMining : MonoBehaviour {
         // we update the timer
         timer += Time.deltaTime;
 
-        if (playerInRange && remainingCrystal > 0)
+        if (!isEmpty)
         {
-            playerMining.isMining = true;
-            if (timer > miningSpeed)
+            if (playerInRange)
             {
-                Mine();
+                playerMining.isMining = true;
+                if (timer > miningSpeed)
+                {
+                    Mine();
+                }
+            }
+            else
+            {
+                playerMining.isMining = false;
+            }
+            if (remainingCrystal <= 0 && !isEmpty)
+            {
+                playerMining.isMining = false;
+                endOfCrystal();
             }
         }
-        else
-        {
-            playerMining.isMining = false;
-        }
+
 	}
 
     void OnTriggerEnter(Collider sc)
@@ -86,6 +105,29 @@ public class CrystalMining : MonoBehaviour {
         // we launch the floating crystal
         GameObject floatingCrystal = (GameObject)Instantiate(flyingCrystal, transform.position, new Quaternion(0,0,0,0));
         floatingCrystal.gameObject.name = "FloatingCrystal";
+
+    }
+
+    void endOfCrystal()
+    {
+        isEmpty = true;
+
+        allEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject enemy in allEnemies)
+        {
+            GameObject burn = (GameObject)Instantiate(fire, enemy.transform.position, new Quaternion(0, 0, 0, 0));
+            Destroy(enemy, 2f);
+            Destroy(burn, 2f);
+        }
+        GameObject aura = (GameObject)Instantiate(crystalAura, transform.position + new Vector3(0,2,0), new Quaternion(0, 0, 0, 0));
+        //GameObject env = (GameObject)Instantiate(crystalEnv, transform.position, new Quaternion(0, 0, 0, 0));
+
+        aura.gameObject.name = "CrystalAura";
+        //env.gameObject.name = "CrystalEnv";
+
+        Destroy(aura, 5f);
+
+        playerMining.getNextWeapon();
 
     }
 }
